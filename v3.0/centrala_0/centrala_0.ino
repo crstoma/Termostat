aaa5541c42aa4ada193bf7614546eec8bf4c29a7
crstoma <smarandachetoma@gmail.com>
2024-12-07 16:56:10 +0200

    update

---
+++ v3.0/centrala_0/centrala_0.ino
@@ -0,0 +1,91 @@
+#include <ESP8266WiFi.h>
+#include <ESP8266WebServer.h>
+
+// Configurare WiFi
+const char* ssid = "EpicMe";
+const char* password = "cristofer27";
+
+// Configurare pin releu
+#define RELAY_PIN D1
+
+// Configurare IP static pentru centrala de comandă
+IPAddress local_ip(192, 168, 0, 100);   // Adresa IP statică
+IPAddress gateway(192, 168, 0, 1);      // Gateway-ul (de obicei, routerul)
+IPAddress subnet(255, 255, 255, 0);     // Mască de subrețea
+
+// Setări pentru server
+ESP8266WebServer server(80);  // Crează server HTTP pe portul 80
+
+// Variabile pentru controlul centralei
+bool heating = false; // Starea centralei (pornită/oprită)
+
+void setup() {
+  Serial.begin(115200);
+  delay(1000);
+  
+  // Configurare pin releu
+  pinMode(RELAY_PIN, OUTPUT);
+  digitalWrite(RELAY_PIN, HIGH);  // Asigură-te că releul este oprit la început
+  
+  // Conectare la WiFi
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("Conectat la WiFi!");
+
+  // Configurare IP static
+  WiFi.config(local_ip, gateway, subnet);
+  Serial.print("IP-ul centralei este: ");
+  Serial.println(WiFi.localIP());
+
+  // Adaugă o rută HTTP pentru a obține starea curentă
+  server.on("/status", HTTP_GET, handleStatusRequest);
+
+  // Configurare rute HTTP
+  server.on("/control", HTTP_GET, handleControlRequest);
+
+  // Pornire server
+  server.begin();
+  Serial.println("Server pornit!");
+}
+
+void loop() {
+  // Servește cererile HTTP
+  server.handleClient();
+}
+
+// Funcție pentru a răspunde la cererile de status
+void handleStatusRequest() {
+  String response = heating ? "Centrala este pornita" : "Centrala este oprita";
+  server.send(200, "text/plain", response);
+}
+
+// Funcție pentru a gestiona cererea de la termostat
+void handleControlRequest() {
+  String id = server.arg("id");
+  String state = server.arg("state");
+
+  // Verifică ID-ul termostatului și starea
+  Serial.println("Cerere primită de la termostat:");
+  Serial.print("ID: ");
+  Serial.println(id);
+  Serial.print("Stare: ");
+  Serial.println(state);
+
+  if (id == "1") {
+    if (state == "on") {
+      heating = true;
+      digitalWrite(RELAY_PIN, LOW);  // Pornește centrala
+      Serial.println("Centrala pornită!");
+    } else if (state == "off") {
+      heating = false;
+      digitalWrite(RELAY_PIN, HIGH);   // Oprește centrala
+      Serial.println("Centrala oprită!");
+    }
+  }
+
+  // Răspuns HTTP
+  server.send(200, "text/plain", heating ? "Centrala este pornita" : "Centrala este oprita");
+}
---
+++ v3.0/termostat_0/termostat_0.ino
@@ -0,0 +1,239 @@
+#include <ESP8266WiFi.h>
+#include <ESP8266HTTPClient.h>
+#include <Adafruit_Sensor.h>
+#include <DHT.h>
+#include <Adafruit_SSD1306.h>
+#include <Adafruit_GFX.h>
+#include <Wire.h>
+
+// Configurare WiFi
+const char* ssid = "EpicMe";
+const char* password = "cristofer27";
+
+// Configurare DHT
+#define DHTPIN 4
+#define DHTTYPE DHT22
+DHT dht(DHTPIN, DHTTYPE);
+
+// Configurare OLED
+#define SCREEN_WIDTH 128
+#define SCREEN_HEIGHT 64
+#define OLED_RESET -1
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+
+// Configurare pini I2C
+#define SDA_PIN 14
+#define SCL_PIN 12
+
+// Configurare butoane
+#define BTN_UP 0
+#define BTN_DOWN 5
+#define BTN_SELECT 13
+
+// Configurare centrale de comandă
+const char* boilerIP = "192.168.0.100"; // IP-ul centralei de comandă
+const int boilerPort = 80;
+const int thermostatID = 1; // ID unic al acestui termostat
+
+// Variabile pentru temperatură
+float targetTemp = 22.0;
+float hyst = 0.2;
+bool heating = false;
+
+// Variabile pentru gestionarea butoanelor
+unsigned long lastButtonCheck = 0; // Momentul ultimei verificări
+const unsigned long buttonCheckInterval = 200; // Interval mai lung pentru debouncing
+bool btnUpPressed = false;
+bool btnDownPressed = false;
+bool btnSelectPressed = false;
+
+// Stare meniu
+bool inMenu = false;
+
+void setup() {
+  Serial.begin(115200);
+
+  // Configurare WiFi
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("Conectat la WiFi!");
+
+  // Inițializare DHT
+  dht.begin();
+
+  // Inițializare I2C și OLED
+  Wire.begin(SDA_PIN, SCL_PIN);
+  if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) {
+    Serial.println("Eroare la inițializarea OLED!");
+    for (;;);
+  }
+
+  // Inițializare butoane
+  pinMode(BTN_UP, INPUT_PULLUP);
+  pinMode(BTN_DOWN, INPUT_PULLUP);
+  pinMode(BTN_SELECT, INPUT_PULLUP);
+
+  // Mesaj inițial pe OLED
+  display.clearDisplay();
+  display.setTextSize(1);
+  display.setTextColor(SSD1306_WHITE);
+  display.setCursor(0, 0);
+  display.println("Termostat pornit!");
+  display.display();
+  delay(2000);
+}
+
+void loop() {
+  checkButtons();
+  // Verifică periodic starea centralei
+  checkBoilerStatus();
+  if (inMenu) {
+    updateDisplayMenu();
+  } else {
+    float temp = dht.readTemperature();
+    float hum = dht.readHumidity();
+
+    if (isnan(temp) || isnan(hum)) {
+      Serial.println("Eroare la citirea senzorului DHT22!");
+      return;
+    }
+
+    // Control histerezis
+    if (temp < (targetTemp - hyst) && !heating) {
+      heating = true;
+      sendSignalToBoiler(true);
+    } else if (temp > (targetTemp + hyst) && heating) {
+      heating = false;
+      sendSignalToBoiler(false);
+    }
+
+    // Actualizare afișaj OLED
+    display.clearDisplay();
+    display.setTextSize(2);
+    display.setTextColor(SSD1306_WHITE);
+    display.setCursor(0, 0);
+    display.printf("T: %.1f C\n", temp);
+    display.printf("U: %.1f %%\n", hum);
+    display.printf("SET:%.1f C\n", targetTemp);
+    display.setTextSize(1);
+    display.printf("Centrala: %s\n", heating ? "Pornit" : "Oprit");
+    display.display();
+
+    delay(2000);
+  }
+}
+
+void checkButtons() {
+  if (millis() - lastButtonCheck >= buttonCheckInterval) {
+    lastButtonCheck = millis();
+
+    // Citire stare butoane
+    bool btnUpState = digitalRead(BTN_UP) == LOW;
+    bool btnDownState = digitalRead(BTN_DOWN) == LOW;
+    bool btnSelectState = digitalRead(BTN_SELECT) == LOW;
+
+    // Gestionare butoane
+    if (btnUpState && !btnUpPressed) {
+      btnUpPressed = true;
+      if (inMenu) {
+        targetTemp += 0.5;
+        if (targetTemp > 30.0) targetTemp = 30.0;
+      }
+    } else if (!btnUpState) {
+      btnUpPressed = false;
+    }
+
+    if (btnDownState && !btnDownPressed) {
+      btnDownPressed = true;
+      if (inMenu) {
+        targetTemp -= 0.5;
+        if (targetTemp < 19.0) targetTemp = 19.0;
+      }
+    } else if (!btnDownState) {
+      btnDownPressed = false;
+    }
+
+    if (btnSelectState && !btnSelectPressed) {
+      btnSelectPressed = true;
+      inMenu = !inMenu;
+    } else if (!btnSelectState) {
+      btnSelectPressed = false;
+    }
+  }
+}
+
+void updateDisplayMenu() {
+  display.clearDisplay();
+  display.setTextSize(1);
+  display.setTextColor(SSD1306_WHITE);
+
+  if (inMenu) {
+    display.setCursor(0, 0);
+    display.println("SETARE TEMPERATURA");
+    display.setCursor(0, 20);
+    display.setTextSize(2);
+    display.printf("SET:%.1f C\n", targetTemp);
+    display.setTextSize(1);
+    display.println("UP/DOWN: Modifica");
+    display.println("SELECT: Salveaza");
+  }
+
+  display.display();
+}
+
+void sendSignalToBoiler(bool state) {
+  if (WiFi.status() == WL_CONNECTED) {
+    WiFiClient client;
+    HTTPClient http;
+
+    String url = String("http://") + boilerIP + ":" + boilerPort +
+                 "/control?id=" + thermostatID +
+                 "&state=" + (state ? "on" : "off");
+
+    if (http.begin(client, url)) {
+      int httpCode = http.GET();
+      if (httpCode > 0) {
+        Serial.println("Semnal trimis către centrală!");
+      } else {
+        Serial.println("Eroare la trimiterea semnalului!");
+      }
+      http.end();
+    } else {
+      Serial.println("Nu s-a putut inițializa conexiunea HTTP!");
+    }
+  } else {
+    Serial.println("Nu există conexiune WiFi!");
+  }
+}
+
+void checkBoilerStatus() {
+  if (WiFi.status() == WL_CONNECTED) {
+    WiFiClient client;
+    HTTPClient http;
+
+    String url = String("http://") + boilerIP + "/status";  // Ruta care returnează statusul centralei
+
+    if (http.begin(client, url)) {
+      int httpCode = http.GET();
+      if (httpCode > 0) {
+        String response = http.getString();
+        if (response.indexOf("pornita") >= 0) {
+          heating = true;
+        } else {
+          heating = false;
+        }
+        Serial.println("Status centrala: " + response);
+      } else {
+        Serial.println("Eroare la cererea statusului centralei");
+      }
+      http.end();
+    } else {
+      Serial.println("Nu s-a putut inițializa conexiunea HTTP!");
+    }
+  } else {
+    Serial.println("Nu există conexiune WiFi!");
+  }
+}
